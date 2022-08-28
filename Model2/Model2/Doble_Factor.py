c643256fcc633f968ec12cddb3b97b542f62e71e
RogelioDaniel <rogelio.daniel@hotmail.es>
2022-08-27 23:30:47 -0500

    Creacion de codigo de las clases en Java y Python

---
+++ Model2/Model2/Doble_Factor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Doble_Factor:
+    def __init__(self):
+        self.Id_usuario = None
+        self.Clave_actual = None
+        self.Clave_anterior = None
+        self.Attribute1 = None
+
+    def Aceptar(self, ):
+        pass
+
+    def Rechazar(self, ):
+        pass
+
